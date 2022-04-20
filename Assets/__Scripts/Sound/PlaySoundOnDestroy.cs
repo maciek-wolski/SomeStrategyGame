47347dfb4f0a4a2e1c544f17a83fc6fa5f2d6c39
Maciek <maciek.wol16@gmail.com>
2022-04-20 23:16:49 +0200

    +play sound if the object is visible in maincamera

--- Assets/__Scripts/Sound/PlaySoundOnDestroy.cs
+++ Assets/__Scripts/Sound/PlaySoundOnDestroy.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class PlaySoundOnDestroy : MonoBehaviour
 {
     [SerializeField] private AudioClip clip = null;
+    private Camera mainCamera;
 
+    private void Awake() {
+        mainCamera = Camera.main;
+    }
     private void OnDestroy() {
-        if (SoundManager.instance != null) { 
+        if (SoundManager.instance != null && IsVisibleToMainCamera()) { 
             SoundManager.instance.PlaySound(clip);
         }
+    }
+
+    private bool IsVisibleToMainCamera() {
+        Vector3 viewPos = mainCamera.WorldToViewportPoint(gameObject.transform.position);
+        if (viewPos.x >= 0 && viewPos.x <= 1 
+            && viewPos.y >= 0 && viewPos.y <= 1
+            && viewPos.z > 0) {
+                
+            return true;
+        }
+
+        return false;
     }
 }
--- Assets/__Scripts/Sound/PlaySoundOnStart.cs
+++ Assets/__Scripts/Sound/PlaySoundOnStart.cs
@@ -6,7 +6,26 @@ public class PlaySoundOnStart : MonoBehaviour
 {
     [SerializeField] private AudioClip clip;
 
+    Camera mainCamera;
+
+    private void Awake() {
+        mainCamera = Camera.main;
+    }
+
     private void Start() {
-        SoundManager.instance.PlaySound(clip);
+        if (IsVisibleToMainCamera())
+            SoundManager.instance.PlaySound(clip);
+    }
+
+    private bool IsVisibleToMainCamera() {
+        Vector3 viewPos = mainCamera.WorldToViewportPoint(gameObject.transform.position);
+        if (viewPos.x >= 0 && viewPos.x <= 1 
+            && viewPos.y >= 0 && viewPos.y <= 1
+            && viewPos.z > 0) {
+                
+            return true;
+        }
+
+        return false;
     }
 }
